f4bcbd3ed4bdb4b19fb33e9954f44be743dae13a
linusmuema <linus.m.muema@gmail.com>
2022-06-15 12:16:01 +0300

    Initial Commit

---
+++ Flixy/FlixyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlixyApp.swift
+//  Flixy
+//
+//  Created by Linus Muema on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlixyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
